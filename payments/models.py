36afef8d1d4b3d037d0301b38196f0d6da31e533
Norilka <NorilkaRus@gmail.com>
2024-01-22 22:24:28 +0300

    Homework 24.2

---
+++ payments/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from education.models import Course, Lesson
+from users.models import User, NULLABLE
+
+# Create your models here.
+
+class Payment(models.Model):
+
+    PERIODICITY_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь")
+    date = models.DateField(verbose_name="дата оплаты")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, verbose_name="оплаченный курс")
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, **NULLABLE, verbose_name="оплаченный урок")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="сумма оплаты")
+    payment_method = models.CharField(max_length=10, choices=PERIODICITY_CHOICES, verbose_name="способ оплаты")
+
+    def __str__(self):
+        return f'{self.user}: {self.amount} ({self.date})'
+
+    class Meta:
+        verbose_name = "платеж"
+        verbose_name_plural = "платежи"
